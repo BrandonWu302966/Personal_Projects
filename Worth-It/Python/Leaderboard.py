5de4b66e870bf1788b142a41ae9fa5ca6d54419c
Brandon Wu <wu302966@gmail.com>
2022-10-16 14:06:43 -0700

    j

---
+++ Worth-It/Python/Leaderboard.py
@@ -0,0 +1,3 @@
+from Worth_packs import csv_to_array
+
+csv_to_array("Big_Library.csv")
---
+++ Worth-It/Python/Run.py
@@ -0,0 +1,42 @@
+
+# %%
+import matplotlib.pylab as plt
+import matplotlib as mpl
+import numpy as np
+import datascience as ds
+import pandas as pd
+from Worth_packs import array_to_table
+from Worth_packs import table_to_array
+from Worth_packs import array_to_csv
+from Worth_packs import csv_to_array
+# %% 
+
+final_array = np.genfromtxt("Big_Library.csv", delimiter = ",", dtype = 'str_')
+Final_Lib = array_to_table(final_array).sample(with_replacement=False)
+
+i = 0
+error_array = np.array([])
+swiped_dish = np.array([])
+guess_array = np.array([])
+while True:
+    if i >= Final_Lib.num_rows:
+        break
+    cont = input("Show Food Item?: ")
+    if cont == "No":
+        break
+    print(np.array(Final_Lib.select(1,2).row(i)))
+    approximation = int(input("What do you think is the price of this dish: "))
+    guess_array = np.append(guess_array, approximation)
+    swiped_dish = np.append(swiped_dish, np.array(Final_Lib.row(i)).item(1))
+    exact = float(np.array(Final_Lib.row(i)).item(3))
+    error_array = np.append(error_array, "%" + str(((approximation - exact) / exact)*100))
+    i += 1
+
+error_table = ds.Table().with_columns("Menu Item", swiped_dish, "Guessed Price", guess_array, "Error", error_array)
+match = table_to_array(Final_Lib.join('Menu Item', error_table).sort("Error", descending = True))
+array_to_csv(match, "Matches.csv")
+
+output = Final_Lib.join("Menu Item", error_table).sort("Error", descending=True)
+print(output)
+
+
