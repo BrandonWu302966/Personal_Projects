55160d6c0d6192419ee5d5322a9d683c63f382db
Brandon Wu <wu302966@gmail.com>
2022-10-16 14:05:28 -0700

    Worthitfinal

---
+++ Worth-It/main.js
@@ -0,0 +1,11 @@
+function record(e) {
+    e.preventDefault();
+    // TODO: Fill in the rest 
+    var price = document.getElementById("Price").value
+    var price = document.getElementById("")
+    console.log("Guess: " + price)
+
+    alert("Guess Saved!" + " $" + price)
+    displayNextImage()
+
+}
